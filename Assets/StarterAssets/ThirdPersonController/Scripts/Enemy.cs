2fa3daca2b51cd3442acf89deca429137d7ab4cc
Mike1805 <78641062+Mike1805@users.noreply.github.com>
2023-02-05 09:54:05 +0700

    Add Character

---
+++ Assets/StarterAssets/ThirdPersonController/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(Player.position);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
---
+++ Assets/TriggerEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnemy : MonoBehaviour
+{
+    [SerializeField] GameObject m_Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Enemy.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            m_Enemy.SetActive(true);
+        }
+    }
+}
